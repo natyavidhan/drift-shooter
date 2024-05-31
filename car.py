905e0b4732beb68459b356c7a543534208168e5b
FloatingComet62 <shubh2007rai@gmail.com>
2024-05-31 09:24:58 +0530

    moving shit around

--- car.py
+++ car.py
@@ -1,11 +1,9 @@
-import pygame, math
+import pygame
+from math import sin, cos
 
 from objects import Rectangle
 from util import Color, Vec
-
-FRICTION = 0.035
-ACCELERATION = 0.05
-TERMINAL_VELOCITY = 1
+from consts import ACCELERATION, FRICTION, TERMINAL_VELOCITY, TURNING_ANGLE, WINDOW_DIMENSION
 
 def sgn(x):
     if x > 0:
@@ -21,14 +19,13 @@ class Car(Rectangle):
         self.vel = Vec(0, 0)
         self.acc = Vec(0, 0)
         self.accelerating = False
-        super().__init__(Vec(100, 800), Vec(5, 10), color)
+        super().__init__(Vec(WINDOW_DIMENSION.w//2, WINDOW_DIMENSION.h//2), Vec(5, 10), color)
     
     def assign_acc(self, amplitude):
-        self.acc.x = amplitude * math.sin(-self.rotation.radian)
-        self.acc.y = amplitude * math.cos(-self.rotation.radian)
+        self.acc.x = amplitude * sin(-self.rotation.radian)
+        self.acc.y = amplitude * cos(-self.rotation.radian)
     
     def tick(self):
-        print(self.vel.sep())
         # friction
         if not self.accelerating:
             self.acc.x = -sgn(self.vel.x)*min(FRICTION, abs(self.vel.x))
@@ -39,7 +36,7 @@ class Car(Rectangle):
         self.vel.x = sgn(self.vel.x)*min(TERMINAL_VELOCITY, abs(self.vel.x))
         self.vel.y = sgn(self.vel.y)*min(TERMINAL_VELOCITY, abs(self.vel.y))
 
-        self.center.x, self.center.y = Vec.add(self.center, self.vel).sep()
+        self.center = Vec.add(self.center, self.vel)
         self.accelerating = False
 
     def event(self):
@@ -52,6 +49,6 @@ class Car(Rectangle):
             self.assign_acc(ACCELERATION - FRICTION)
 
         if keys[pygame.K_a]:
-            self.rotation.offset(-math.pi/12)
+            self.rotation.offset(-TURNING_ANGLE)
         elif keys[pygame.K_d]:
-            self.rotation.offset(math.pi/12)
+            self.rotation.offset(TURNING_ANGLE)
--- consts.py
+++ consts.py
@@ -1,3 +1,9 @@
+from math import tau
 from util import Vec
 
 WINDOW_DIMENSION = Vec(1600, 900)
+
+FRICTION = 0.02
+ACCELERATION = 0.05
+TERMINAL_VELOCITY = 1
+TURNING_ANGLE = tau/24
