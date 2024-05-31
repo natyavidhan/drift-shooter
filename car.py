6fc3cfe17fb271202409e7ee9bb4d08ebabd23fb
FloatingComet62 <shubh2007rai@gmail.com>
2024-05-31 23:00:32 +0530

    server + client side

--- car.py
+++ car.py
@@ -2,7 +2,7 @@ import pygame
 from math import sin, cos
 
 from objects import Rectangle
-from util import Color, Vec
+from util import Angle, Color, Vec
 from consts import ACCELERATION, FRICTION, TERMINAL_VELOCITY, TURNING_ANGLE, WINDOW_DIMENSION
 
 def sgn(x):
@@ -13,18 +13,19 @@ def sgn(x):
     else:
         return 0
 
-class Car(Rectangle):
+class ServerCar:
     def __init__(self, name, color: Color) -> None:
         self.name = name
         self.vel = Vec(0, 0)
         self.acc = Vec(0, 0)
         self.accelerating = False
-        super().__init__(Vec(0, 0), Vec(25, 50), color)
+        self.center = Vec(0, 0)
+        self.rotation = Angle(degree=0)
     
     def assign_acc(self, amplitude):
         self.acc.x = amplitude * sin(-self.rotation.radian)
         self.acc.y = amplitude * cos(-self.rotation.radian)
-    
+
     def tick(self):
         # friction
         if not self.accelerating:
@@ -43,21 +44,49 @@ class Car(Rectangle):
         
         self.center = Vec.add(self.center, self.vel)
         self.accelerating = False
-
-    def event(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            self.accelerating = True
-            self.assign_acc(-ACCELERATION + FRICTION)
-        if keys[pygame.K_s]:
-            self.accelerating = True
-            self.assign_acc(ACCELERATION - FRICTION)
-
+    
+    def event_w(self):
+        self.accelerating = True
+        self.assign_acc(-ACCELERATION + FRICTION)
+    
+    def event_s(self):
+        self.accelerating = True
+        self.assign_acc(ACCELERATION - FRICTION)
+    
+    def event_a(self):
         vel = self.vel.mod()
         if vel == 0:
             return
+        self.rotation.offset(radian=-TURNING_ANGLE * vel)
+    
+    def event_d(self):
+        vel = self.vel.mod()
+        if vel == 0:
+            return
+        self.rotation.offset(radian=TURNING_ANGLE * vel)
+    
+    def update_client(self):
+        pass
+        # send position
+        # send color
+
+class Car(Rectangle):
+    def __init__(self, name, color: Color) -> None:
+        self.name = name
+        super().__init__(Vec(0, 0), Vec(25, 50), color)
+    
+    def event(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            pass
+            # socket event_w
+        if keys[pygame.K_s]:
+            pass
+            # socket event_s
 
         if keys[pygame.K_a]:
-            self.rotation.offset(radian=-TURNING_ANGLE * vel)
+            pass
+            # socket event_a
         elif keys[pygame.K_d]:
-            self.rotation.offset(radian=TURNING_ANGLE * vel)
+            pass
+            # socket event_d
